2b960cb2f6271b3613c83a1439fef89575e985ee
nannasagen <82771340+nannasagen@users.noreply.github.com>
2021-06-29 15:07:06 +0200

    Add files via upload

---
+++ calendarView.js
@@ -0,0 +1,40 @@
+let html = `
+<div class="calendarFrame"></div>
+<div id="showMonth">June</div>
+
+<div class="week1" id="">1</div>
+<div class="week1" id="">2</div>
+<div class="week1" id="">3</div>
+<div class="week1" id="">4</div>
+<div class="week1" id="">5</div>
+<div class="week1" id="">6</div>
+<div class="week1" id="">7</div>
+
+<div class="week2" id="">8</div>
+<div class="week2" id="">9</div>  
+<div class="week2" id="">10</div>
+<div class="week2" id="">11</div>
+<div class="week2" id="">12</div>
+<div class="week2" id="">13</div>
+<div class="week2" id="">14</div>
+
+<div class="week3" id="">15</div>
+<div class="week3" id="">16</div>
+<div class="week3" id="">17</div>
+<div class="week3" id="">18</div>
+<div class="week3" id="">19</div>
+<div class="week3" id="">20</div>
+<div class="week3" id="">21</div>
+
+<div class="week4" id="">22</div>
+<div class="week4" id="">23</div>
+<div class="week4" id="">24</div>
+<div class="week4" id="">25</div>
+<div class="week4" id="">26</div>
+<div class="week4" id="">27</div>
+<div class="week4" id="">28</div>
+
+<div class="restOfMonth" id="">29</div>
+<div class="restOfMonth" id="">30</div>
+<div class="restOfMonth" id="">31</div>
+`
